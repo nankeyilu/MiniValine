ea834a7755541c094b03a9e4abc5f7184aa2efd0
nankeyilu <nankeyilu@protonmail.com>
2021-04-11 23:34:15 +0800

    Prepare for custom release

--- src/Default.js
+++ src/Default.js
@@ -1,10 +1,12 @@
 export const VERSION = '5.1.7'
+export const customVERSION = '5.1.7.2'
 export const MVUrl = 'https://cdn.jsdelivr.net/npm/minivaline@' + VERSION
+export const customMVUrl = 'https://cdn.jsdelivr.net/gh/nankeyilu/MiniValine@' + customVERSION
 export const avatarUrl = 'https://secure.gravatar.com/avatar'
 export const avatarD = 'robohash'
 export const EUrl = [MVUrl + '/imgs/alus', MVUrl + '/imgs/qq', MVUrl + '/imgs/Bilibilis', MVUrl + '/imgs/tieba', MVUrl + '/imgs/twemoji', MVUrl + '/imgs/weibo', MVUrl + '/imgs/funny']
 //export const pf = 'https://cdn.polyfill.io/v3/polyfill.min.js?features=es6'
-export const i18nUrl = MVUrl + '/i18n/'
+export const i18nUrl = customMVUrl + '/i18n/'
 export const DEHash = '9e63c80900d106cbbec5a9f4ea433a3e'
 export const MaxNL = 6
 export const PS = 6
